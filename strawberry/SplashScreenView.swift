8304da140b58beda6e2c31d36cc465451f4e5061
vladislvk <vladislavkim566@gmail.com>
2024-10-11 19:12:50 +0500

    feat:blank

---
+++ strawberry/SplashScreenView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text("loading...")
+                    .font(.title)
+            }
+            .scaleEffect(size)
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 1.2)) {
+                    self.size = 0.9
+                    self.opacity = 1.0
+                }
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.isActive = true
+            }
+        }
+        .fullScreenCover(isPresented: $isActive) {
+            ContentView()
+        }
+    }
+}
+
--- strawberry/strawberryApp.swift
+++ strawberry/strawberryApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct strawberryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
