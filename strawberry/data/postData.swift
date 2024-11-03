53d4d05a3b2b4f8d774d8c07831cc06ec29da8fa
vladislvk <vladislavkim566@gmail.com>
2024-11-03 22:58:08 +0500

    feat:blank

--- strawberry/data/postData.swift
+++ strawberry/data/postData.swift
@@ -4,31 +4,31 @@ import SwiftUI
 let randomData: [Post] = [
     Post(
         username: "Tauke",
-        profilePicture: "profile_placeholder",
-        postImage: "post_placeholder",
+        profilePicture: "imagepost",
+        postImage: "imagepost",
         caption: "Enjoying the beautiful sunset! 🌅",
         likes: 123,
         comments: ["Amazing view!", "Where is this?", "Looks beautiful!"]
     ),
     Post(
         username: "Artyom",
-        profilePicture: "profile_placeholder",
-        postImage: "post_placeholder",
+        profilePicture: "imagepost",
+        postImage: "imagepost",
         caption: "Hiking adventures 🚶‍♂️",
         likes: 456,
         comments: ["Love the outdoors!", "Great shot!", "So inspiring!"]
     ),
     Post(
         username: "Azamat",
-        profilePicture: "profile_placeholder",
-        postImage: "post_placeholder",
+        profilePicture: "imagepost",
+        postImage: "imagepost",
         caption: "Throwback to last summer ☀️",
         likes: 789,
         comments: ["Good times!", "Miss this!", "Can’t wait for summer again!"]
     ),Post(
         username: "Danik",
-        profilePicture: "profile_placeholder",
-        postImage: "post_placeholder",
+        profilePicture: "imagepost",
+        postImage: "imagepost",
         caption: "Throwback to last summer ☀️",
         likes: 1000,
         comments: ["Good times!", "Miss this!", "Can’t wait for summer again!"]
--- strawberry/screens/screenOne.swift
+++ strawberry/screens/screenOne.swift
@@ -1,70 +1,49 @@
 import SwiftUI
 
 struct screenOne: View {
-    @State private var currentIndex = 0
-    @State private var postsToShow: [Post] = []
-    private let loadCount = 3
-    
     var body: some View {
         VStack {
             headerView()
             
             ScrollView(.vertical, showsIndicators: false) {
-                LazyVStack {
-                    storiesView()
-                    
-                    ForEach(postsToShow) { post in
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Image(post.profilePicture)
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                    .clipShape(Circle())
-                                
-                                Text(post.username)
-                                    .font(.headline)
-                            }
-                            .padding(.vertical, 5)
-                            
-                            Image(post.postImage)
+                storiesView()
+                ForEach(randomData) { post in
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(post.profilePicture)
                                 .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: .infinity)
+                                .frame(width: 50, height: 50)
+                                .clipShape(Circle())
                             
-                            Text("❤️ \(post.likes) likes")
-                                .font(.subheadline)
-                                .padding(.top, 5)
-                            
-                            Text(post.caption)
-                                .padding(.top, 2)
-                            
-                            Divider()
+                            Text(post.username)
+                                .font(.headline)
                         }
-                        .padding(.horizontal)
+                        .padding(.vertical, 5)
+
+                        Image(post.postImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: .infinity)
+                        
+
+                        Text("❤️ \(post.likes) likes")
+                            .font(.subheadline)
+                            .padding(.top, 5)
+                        
+                        Text(post.caption)
+                            .padding(.top, 2)
+                        
+                        ForEach(post.comments.prefix(2), id: \.self) { comment in
+                            Text(comment)
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        Divider()
                     }
-                    
-                    if currentIndex < randomData.count {
-                        ProgressView()
-                            .onAppear {
-                                loadMorePosts()
-                            }
-                    }
+                    .padding(.horizontal)
                 }
             }
-        }
-        .onAppear {
-            loadMorePosts()
-        }
-    }
-    
-    private func loadMorePosts() {
-        let newPosts = Array(randomData[currentIndex..<min(currentIndex + loadCount, randomData.count)])
-        postsToShow.append(contentsOf: newPosts)
-        
-        currentIndex += loadCount
-        
-        if currentIndex >= randomData.count {
-            currentIndex = randomData.count
         }
     }
 }
